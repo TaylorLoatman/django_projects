51c9e0380cc3289bc8923fd2e98dc5dbc362c3a0
TaylorLoatman <taylor.loatman@icloud.com>
2022-06-03 14:16:08 -0400

    First Commit

---
+++ mysite/main/forms.py
@@ -0,0 +1,9 @@
+import django
+
+
+from django import forms
+
+class CreateNewList(forms.Form):
+    name = forms.CharField(label="Name", max_length=200)
+    check = forms.BooleanField(required=False)
+    
